e9c0a17690ded0bed5efb85e1ed74f1a4e5f712e
Minigrim0 <grimauflorent@gmail.com>
2023-07-19 16:26:11 +0200

    [Add] basic views

--- apps/minigrim0.py
+++ apps/minigrim0.py
@@ -1,9 +1,21 @@
 from flask import Blueprint, request, url_for, redirect, render_template, flash
 minigrim0 = Blueprint('minigrim0', __name__, template_folder="../templates/")
 
+
 @minigrim0.route("/")
 def home():
     return render_template("minigrim0/index.html")
+
+
+@minigrim0.route("/cv")
+def cv():
+    return render_template("minigrim0/cv.html")
+
+
+@minigrim0.route("/projects")
+def projects():
+    return render_template("minigrim0/projects.html")
+
 
 @minigrim0.app_errorhandler(404)
 def page_not_found(e):
