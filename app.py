4d47d0d5f1afd5c96554e6da9481e06e0034941f
Minigrim0 <grimauflorent@gmail.com>
2023-08-30 15:06:59 +0200

    [Add] Sitemaps + some text

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sitemap import Sitemap
 
 # blueprint import
 from apps.minigrim0 import minigrim0
@@ -13,8 +14,16 @@ def create_app():
     app.register_blueprint(minigrim0)
     # app.register_blueprint(app2, url_prefix="/app2")
     Markdown(app)
+    ext = Sitemap(app=app)
+    app.config["SITEMAP_BLUEPRINT"]
+
+    @ext.register_generator
+    def index():
+        for route in ["minigrim0.home", "minigrim0.cv", "minigrim0.projects"]:
+            yield route, {}
 
     return app
+
 
 if __name__ == "__main__":
     create_app().run()
