c15b26cfec4dc4bfa9f71418826cacf0c040f7cc
Minigrim0 <grimauflorent@gmail.com>
2024-01-14 20:57:19 +0100

    [Add] started updating docker files

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
-DOCKERFILE=docker/docker-compose.yml
-export COMPOSE_PROJECT_NAME="minigrim0-web"
+#!/bin/bash
+
+if [ $1 == "dev" ]; then
+    DOCKERFILE=docker/docker-compose.dev.yml
+    export COMPOSE_PROJECT_NAME="minigrim0-web-dev"
+else
+    DOCKERFILE=docker/docker-compose.yml
+    export COMPOSE_PROJECT_NAME="minigrim0-web"
+fi
+
 docker-compose -f $DOCKERFILE up -d --build
-docker-compose -f $DOCKERFILE restart nginx
+docker-compose -f $DOCKERFILE exec web python manage.py makemigrations --noinput
+docker-compose -f $DOCKERFILE exec web python manage.py migrate --noinput
+docker-compose -f $DOCKERFILE exec web python manage.py collectstatic --noinput
+
+if [ $1 != "dev" ]; then
+    docker-compose -f $DOCKERFILE restart nginx
+fi
