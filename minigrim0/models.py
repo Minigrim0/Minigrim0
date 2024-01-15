0b2c62dc48466b9e2d18ce33d4949a5d438d8f0f
Minigrim0 <grimauflorent@gmail.com>
2024-01-15 15:27:59 +0100

    [Upd] repo model w/ url as primary key

--- minigrim0/models.py
+++ minigrim0/models.py
@@ -77,5 +77,5 @@ class Repository(models.Model):
     description = models.CharField(max_length=256)
     readme = models.TextField()
     homepage = models.CharField(max_length=256, blank=True, null=True)
-    url = models.CharField(max_length=256)
+    url = models.CharField(max_length=256, primary_key=True)
     stars = models.IntegerField()
