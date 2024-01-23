6c0406605c3c934f83528b90fdff242944a4a414
Minigrim0 <grimauflorent@gmail.com>
2024-01-23 19:49:46 +0100

    [Add] started blog app

---
+++ blog/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from colorfield.fields import ColorField
+
+
+class Post(models.Model):
+    """Represents a blog post"""
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    tag = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    """Represents a comment to a blog post"""
+
+    pseudo = models.CharField(max_length=100)
+    email = models.EmailField()
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.pseudo
+
+
+class Tag(models.Model):
+    """Represents a tag for a blog post"""
+
+    name = models.CharField(max_length=100)
+    color = ColorField(default='#FF0000')
+
+    def __str__(self):
+        return self.name
