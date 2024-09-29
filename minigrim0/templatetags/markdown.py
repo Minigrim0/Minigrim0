f571f2511560433205937dc59df208cc6b8d67e9
Minigrim0 <grimauflorent@gmail.com>
2024-09-29 20:57:36 +0200

    started blog frontend with updated markdown

--- minigrim0/templatetags/markdown.py
+++ minigrim0/templatetags/markdown.py
@@ -1,12 +1,40 @@
 from django import template
-from django.template.defaultfilters import stringfilter
+from django.template.loader import render_to_string
+import mistune
+from mistune.plugins.task_lists import task_lists
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
+import logging
 
-import markdown as md
-
+logger = logging.getLogger(__file__)
 register = template.Library()
 
+class HighlightRenderer(mistune.HTMLRenderer):
+    def block_code(self, code, info = None):
+        logger.info("Info: %s", info)
 
-@register.filter()
-@stringfilter
+        if not info:
+            return f"""
+```
+{mistune.escape(code)}
+```
+            """
+
+        if ":" in info:
+            lang, filename = info.split(":")
+            lexer = get_lexer_by_name(lang, stripall=True)
+            formatter = HtmlFormatter()
+            rendered_code = highlight(code, lexer, formatter)
+            lines = len(code.split("\n"))
+            return render_to_string("blog/code_block.html", {code: rendered_code, filename: filename})
+        else:
+            lexer = get_lexer_by_name(info, stripall=True)
+            formatter = HtmlFormatter()
+            return highlight(code, lexer, formatter)
+
+@register.filter
 def markdown(value):
-    return md.markdown(value)
+    renderer = HighlightRenderer()
+    markdown = mistune.Markdown(renderer=renderer, plugins=[task_lists])
+    return markdown(value)
---
+++ misc.md
@@ -0,0 +1,7 @@
+# Minigrim0
+
+## Generating style for syntax highlighting
+
+```bash
+pygmentize -S default -f html -a .highlight > minigrim0/assets/css/syntax.css
+```
