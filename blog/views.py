1424a0169b38b44b634339a8252c7d5548b4af17
Minigrim0 <grimauflorent@gmail.com>
2024-09-29 20:57:04 +0200

    added basic views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, render
+from blog.models import Post
 
-# Create your views here.
+
+def blog_root(request) -> HttpResponse:
+    """Returns a list of published posts"""
+
+    posts = Post.objects.filter(published=True).order_by('-date_posted')
+
+    return render(request, 'blog/index.html', {'posts': posts})
+
+
+def post_detail(request, post_id: int) -> HttpResponse:
+    """Returns a single post if existing andd published, 404 otherwise"""
+
+    post = Post.objects.get(pk=post_id)
+    if post.published is False:
+        return HttpResponse(b"Post not found", status=404)
+
+    return render(request, 'blog/post_detail.html', {'post': post})
