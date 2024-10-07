e9d528c53ebca124ef0659a244b3050ee43010e2
Minigrim0 <grimauflorent@gmail.com>
2024-10-07 14:53:31 +0200

    added default views for posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from blog import views
 
 urlpatterns = [
     path('', views.BlogPostListView.as_view(), name='blog-list'),
+    path('new', views.BlogPostCreateView.as_view(), name='post-new'),
     path('<int:pk>/', views.BlogPostDetailView.as_view(), name='post-detail'),
+    path('<int:pk>/edit', views.BlogPostEditView.as_view(), name='post-edit'),
 ]
