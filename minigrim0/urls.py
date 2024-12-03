1cafec9d0cff29e4890ecf9fd3f8e493c3aa2f89
Minigrim0 <grimauflorent@gmail.com>
2024-12-03 14:45:39 +0100

    addded robots.txt & nginx rules

--- minigrim0/urls.py
+++ minigrim0/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import handler400, handler403, handler404, handler500
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 import minigrim0.views as views
 
@@ -10,6 +11,7 @@ urlpatterns = [
     path("cv/", views.cv, name="cv"),
     path("blog/", include(("blog.urls", "blog"), namespace="blog")),
     path("", views.index, name="index"),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots")
 ]
 
 handler404 = lambda req, *args, **kwargs: views.error(req, 404)
